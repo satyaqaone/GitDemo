360e03682c69598a1371942f6caca24574548d6b
Satya <satyagokavarapu1@gmail.com>
2022-05-15 17:11:33 +0530

    first commit

---
+++ src/SamplePack/Sampleone.java
@@ -0,0 +1,18 @@
+package SamplePack;
+
+public class Sampleone {
+
+    public void test1(){
+
+        System.out.println("test1");
+
+    }
+
+    public void test2(){
+
+        System.out.println("test2");
+
+    }
+
+
+}
